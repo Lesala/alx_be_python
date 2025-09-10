55d1ffa053eaf20cd3698a0989696bb01916a5e3
Lesala <lpmonaheng@usiu.ac.ke>
2025-09-10 19:15:59 +0300

    Add basic_operations file and modified it

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+# addiction operation for the two numbers
+sum = number1 + number2 
+# subtraction operation for the two numbers
+difference = number1 - number2
+# multiplication operation for the two numbers
+product = number1 * number2
+
+# Printing the results 
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
