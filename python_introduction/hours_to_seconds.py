358a8311db2cadb6d9f40f07710f8ca4cbd97ae6
Lesala <lpmonaheng@usiu.ac.ke>
2025-09-10 21:38:21 +0300

    Modified hours_to_seconds file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+# Defining the variable for hours to seconds conversion
+hours = 2
+# Calculating the number of seconds in the given hours
+seconds = hours * 3600
+# Displaying the result
+print(f'{hours} hours is {seconds} secoconds')
