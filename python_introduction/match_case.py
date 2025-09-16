a7496ae043f87a1b5b4df33eae9e3981cd9aeb3e
Lesala <lpmonaheng@usiu.ac.ke>
2025-09-16 10:48:24 +0300

    Add match case file and practices

---
+++ python_introduction/match_case.py
@@ -0,0 +1,20 @@
+# This program prompts the user to enter a day of the week and uses a match-case statement to print a message based on the input.
+day = str(input("Enter a day of the week: ")).lower()
+
+match day:
+    case "monday":
+       print("Damn, it's Monaday!")
+    case "tuesday":
+        print("It's Tuesday, let's keep grinding.")
+    case "wedsday":
+        print("It's wednesday, well begun is half done.")
+    case "thursday":
+        print("It's Thursday, let's wrap up the week.")
+    case "friday":
+        print("It's friday, let's finish strong.")
+    case _:
+        print("""It's the weekend , time to relax and recharge
+              and spend quality time with family""")
+    
+
+
